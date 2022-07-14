67cd5fefdaf1e158c6d87dacd7b23714c7ba50a4
PolkanDo <mrmarmon@mail.ru>
2022-07-14 17:47:55 +0300

    created project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Project(models.Model):
+    project_id = models.AutoField(primery_key=True)
+    project_name = models.CharField(
+        max_length=250,
+        verbose_name="Project Name",
+        unique=True
+    )
+    project_owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Project Author',
+        related_name='projects',
+    )
+    project_field = models.CharField(
+        verbose_name="Project Description"
+    )
+
