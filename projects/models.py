4556f53906baf5da1718875ae61db9ddbfe00bd2
PolkanDo <mrmarmon@mail.ru>
2022-07-14 17:50:05 +0300

    changed project model

--- projects/models.py
+++ projects/models.py
@@ -20,4 +20,17 @@ class Project(models.Model):
     project_field = models.CharField(
         verbose_name="Project Description"
     )
+    project_pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Publication_date'
+    )
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return f'Project: {self.project_name}, author: {self.project_owner}'
 
