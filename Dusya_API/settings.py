d6aaec8eeb7bd6b399486f2bb48bc3591dc5b59f
PolkanDo <mrmarmon@mail.ru>
2022-07-19 17:41:06 +0300

    added urls for projects

--- Dusya_API/settings.py
+++ Dusya_API/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/backend_static/'
+STATIC_ROOT = str(BASE_DIR / 'backend_static')
+
+MEDIA_URL = '/backend_media/'
+MEDIA_ROOT = str(BASE_DIR / 'backend_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
