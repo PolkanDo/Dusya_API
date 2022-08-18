b6f947ad6e6f8544d31d786b86766ba07333dabf
PolkanDo <mrmarmon@mail.ru>
2022-08-18 15:58:40 +0300

    changed UserAdminConfig

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class UserAdminConfig(UserAdmin):
+
+    ordering = ("-last_name",)
+    list_display = ("email", "first_name", "last_name",
+                    "is_active", "is_staff")
+
+
+admin.site.register(User, UserAdminConfig)
